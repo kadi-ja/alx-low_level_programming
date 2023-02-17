3e6827652b19facd6d37084dc51ee2740de53510
kadi-ja <khadija.boustila.96@gmail.com>
2023-02-17 09:32:16 -0800

    fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,11 +10,11 @@ char c;
 float f;
 long int l;
 long long int d;
-printf("Size of a char: %zu byte(s)\n",sizeof(c));
- printf("Size of an int: %zu byte(s)\n",sizeof(i));
- printf("Size of a long int: %zu byte(s)\n",sizeof(l));
-printf("Size of a long long int: %zu byte(s)\n",sizeof(d));
- printf("Size of a float: %zu byte(s)\n",sizeof(f));
+printf("Size of a char: %zu byte(s)\n", sizeof(c));
+printf("Size of an int: %zu byte(s)\n", sizeof(i));
+printf("Size of a long int: %zu byte(s)\n", sizeof(l));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
+printf("Size of a float: %zu byte(s)\n", sizeof(f));
 return (0);
 }
 
