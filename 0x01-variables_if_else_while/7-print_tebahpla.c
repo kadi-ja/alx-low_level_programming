8ab3eb1827b3b10606f05c0dd23e11653171154a
kadi-ja <khadija.boustila.96@gmail.com>
2023-02-21 13:08:44 -0800

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - prints the lowercase alphabet in reverse
+*Return: always 0
+*/
+int main(void)
+{
+char c;
+for (c = 'z'; c >= 'a'; c--)
+putchar(c);
+putchar('\n');
+return (0);
+}
