cbbc5a475c37d9fb25cb44411d1f5fcb685ce149
kadi-ja <khadija.boustila.96@gmail.com>
2023-02-22 14:41:39 -0800

    hotfix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
-#include <unistd.h>
-#include <stdio.h>
+#include "main.h"
+#include "stdio.h"
 /**
-*main - prints _putchar
-*Return: always 0
-*/
-int _putchar(char c)
-  {
-	return (write(1, &c, 1));
-}
+ *main - print _putchar
+ *Return: always 0
+ */
 int main(void)
 {
-char *w = "_putchar";
- while (*w)
-   {
-_putchar(*w);
-     w++;
-}
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
 putchar('\n');
 return (0);
 }
