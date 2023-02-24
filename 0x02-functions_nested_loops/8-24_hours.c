9678001f493ac94735dc0e0665d30d18ebf8f1a9
kadi-ja <khadija.boustila.96@gmail.com>
2023-02-24 12:20:53 -0800

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *jack_bauer - a function that prints every minute of the day 
+ *
+ *Return: always 0
+ */
+void jack_bauer(void)
+{
+int h, m;
+for (h = 0; h <= 23; h++)
+{
+for (m = 0; m <= 59; m++)
+{
+return (h':'m);
+}
+return ('\n');
+}
+}
