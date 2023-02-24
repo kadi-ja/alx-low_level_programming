a6d76d2b1fa511bde6e36cb381944c6b424e97bf
kadi-ja <khadija.boustila.96@gmail.com>
2023-02-24 14:05:11 -0800

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+void times_table(void)
+{
+  int a;
+  int b;
+  int c = a * b;
+  for (a = 0; a >= 9; a++)
+    {
+      for (b = 0; b>= 9; b++)
+	{
+	  _putchar(c);
+	  _putchar(',');
+	  _putchar(' ');
+}
+      _putchar('\n');
+    }
+}
