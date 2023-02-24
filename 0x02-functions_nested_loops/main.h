bd6b9950f7e528f657a6bf61598649e05d7c9375
kadi-ja <khadija.boustila.96@gmail.com>
2023-02-24 10:29:05 -0800

    task 7

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -7,4 +5,3 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-#endif
