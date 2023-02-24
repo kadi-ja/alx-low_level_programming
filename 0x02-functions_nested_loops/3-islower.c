3e5c19db430f17d46b3e88f2e29f82e6b754d241
kadi-ja <khadija.boustila.96@gmail.com>
2023-02-24 07:19:24 -0800

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+ *_islower - checks for lowercase character
+ *
+ *Return: always 0
+ */
+int _islower(int c)
+{
+  if (c > 96 && c < 123)
+    {
+return (1);
+    }
+  else
+    {
+    return (0);
+    }
+}
