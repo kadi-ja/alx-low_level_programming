fefb8a96ec4255a1b75b1a89227d5ee690ce7208
kadi-ja <khadija.boustila.96@gmail.com>
2023-02-25 04:30:01 -0800

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_isupper - a function that checks for uppercase character.
+ *@c: int
+ *Return: alawys 0
+ */
+int _isupper(int c)
+{
+  if ((c > 64) && (c < 91)
+    {
+      return (1);
+    }
+    else
+      {
+	return (0);
+      }
+}
