06a67a45c798ca1a01c2218794dd7a8657837037
kadi-ja <khadija.boustila.96@gmail.com>
2023-02-25 09:51:35 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *print_numbers - a function that prints the numbers, from 0 to 9
+ *Return: always 0
+ */
+void print_numbers(void)
+{
+  int a;
+  for (a = 48; a >= 57; i++)
+    {
+      _putchar(a);
+    }
+  _putchar('\n');
+}
