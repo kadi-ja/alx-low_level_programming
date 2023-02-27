ae038bb303101ca456741514c04f8d0f32f31717
dija <96546980+kadi-ja@users.noreply.github.com>
2023-02-28 00:56:01 +0100

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,13 +12,13 @@ for (i = 0; i < n; i++)
 {
 if (i == n - 1)
 {
-printf("%d", a[i]);
+_putchar(a[i]);
 }
 else
 {
-printf("%d", a[i]);
-printf(", ");
+_putchar(a[i]);
+_putchar(', ');
 }
 }
-printf("\n");
+_putchar('\n');
 }
