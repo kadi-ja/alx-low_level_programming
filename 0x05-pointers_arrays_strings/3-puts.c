3e4a4d35437516a35a3656e2a6f50480740f4c2e
kadi-ja <khadija.boustila.96@gmail.com>
2023-02-27 15:15:15 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *_puts -  prints a string, followed by a new line, to stdout.
+ *@str: string data type
+ */
+void _puts(char *str)
+{
+  _putchar(*str);
+  _putchar('\n');
+}
