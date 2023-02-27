039f3553e392e3ef8461d61bd6d3ebcf13d89794
dija <96546980+kadi-ja@users.noreply.github.com>
2023-02-28 00:31:46 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,3 +3,12 @@
  *print_rev - prints a string, in reverse, followed by a new line.
  *@s: string data type 
  */
+void print_rev(char *s)
+{
+ int i;
+  for ( s[i] != '\0'; i = 0; i++)
+  {
+   _putchar(s[i]);
+  }
+ _putchar('\n');  
+}
