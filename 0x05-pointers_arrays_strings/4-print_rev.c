6c2f0bf6ffc863b6c36d9491a9f4c8ea8f14eff5
dija <96546980+kadi-ja@users.noreply.github.com>
2023-02-28 00:39:59 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,14 +5,14 @@
  */
 void print_rev(char *s)
 {
- int i;
-  for (i = 0; s[i] != '\0'; i++)
-  {
-   ;
-  }
- for (i--; i >= 0; i--)
- {
-   _putchar(s[i]);
-  }
- _putchar('\n');  
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+;
+}
+for (i--; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
 }
