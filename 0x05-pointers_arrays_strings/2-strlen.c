fb5e6f51cab46ccd53fa3a03bf735810ebc98d9a
kadi-ja <khadija.boustila.96@gmail.com>
2023-02-27 14:47:03 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_strlen - a function that returns the length of a string.
+ *@s: string data type
+ *Return: always 0
+ */
+int _strlen(char *s)
+{
+  int i;
+  for (i = 0; s[i] != '\0'; i++)
+    {
+      _putchar(i);
+    }
+  return (0);
+}
