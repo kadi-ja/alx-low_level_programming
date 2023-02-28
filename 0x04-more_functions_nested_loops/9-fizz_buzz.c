97cbd3b2194d877c9f495742a61f657f2a9fa0cd
dija <96546980+kadi-ja@users.noreply.github.com>
2023-02-28 01:10:28 +0100

    Create 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - prints numbers and some expressions
+ * Return: 0 if code succesfluly ran
+ */
+int main(void)
+{
+int b;
+b = 1;
+while (b <= 100)
+{
+if (b % 3 == 0 && b % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else if (b % 3 == 0)
+{
+printf("Fizz");
+}
+else if (b % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", b);
+}
+if (b != 100)
+{
+printf(" ");
+}
+b++;
+}
+printf("\n");
+
+return (0);
+}
