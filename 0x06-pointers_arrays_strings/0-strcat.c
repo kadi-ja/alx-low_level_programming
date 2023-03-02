c3b59f97707e7ccefbe1a4f35854e7f050b04054
dija <96546980+kadi-ja@users.noreply.github.com>
2023-03-02 20:58:37 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  *_strcat -  a function that concatenates two strings.
  *@dest: data type
@@ -9,10 +8,10 @@
 char *_strcat(char *dest, char *src)
 {
   int i, j;
- for(i=0;dest[i]!='\0';i++);
- for(int j=0;src[j]!='\0';j++)  
+ for (i = 0; dest[i] != '\0'; i++);
+ for (j = 0; src[j] != '\0'; j++)  
        {     
-        dest[i]=src[j];  
+        dest[i] = src[j];  
         i++;  
     } 
   return (dest);
