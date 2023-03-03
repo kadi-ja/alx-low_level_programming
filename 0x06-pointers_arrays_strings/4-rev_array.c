66b0fc42542f1415815f20c6b54ad176f778dc8f
dija <96546980+kadi-ja@users.noreply.github.com>
2023-03-03 17:40:09 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,15 +2,15 @@
 /**
 *reverse_array - a function that reverses the content of an array of integers.
 *@a: data type
-*@n: data type 
+*@n: data type
 */
 void reverse_array(int *a, int n)
 {
- int i, j;
-  for (i = 0; i < (n / 2); i++)
-  {
-  i = a[i];
-    a[i] = a[n - i - 1];
-    a[n - i - 1] = c;
+int i, j;
+for (i = 0; i < (n / 2); i++)
+{
+j = a[i];
+a[i] = a[n - i - 1];
+a[n - i - 1] = j;
 }
-        }
+}
