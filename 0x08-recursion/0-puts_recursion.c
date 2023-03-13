25965ec10a5eb98f01210a86c71999ef10e00017
kadi-ja <khadija.boustila.96@gmail.com>
2023-03-13 07:58:33 -0700

    puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *_puts_recursion -  a function that prints a string.
+ *@s: data type 
+ */
+void _puts_recursion(char *s)
+{
+s = "Puts with recursion"
+  _putchar (s);
+}
