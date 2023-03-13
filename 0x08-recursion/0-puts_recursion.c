75572c3527d7a823391facabd5a813e8c9a059ba
dija <96546980+kadi-ja@users.noreply.github.com>
2023-03-13 16:46:25 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,10 @@
 #include "main.h"
+
 /**
  *_puts_recursion -  a function that prints a string.
  *@s: data type 
  */
+
 void _puts_recursion(char *s)
 {
 if (*s)
@@ -10,4 +12,5 @@ if (*s)
 _puts_recursion(s + 1);
 _putchar (*s);
 }
+_putchar ('\n');
 }
