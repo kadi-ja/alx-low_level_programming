a8772a7997c4d8d537e13d5b641a322d6cd67221
dija <96546980+kadi-ja@users.noreply.github.com>
2023-03-14 15:47:00 +0100

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,18 +9,17 @@
 
 int _pow_recursion(int x, int y)
 {
-
-   if (y < 0)
-   {
-       return (-1);
-   }
-  else if (y == 0)
-   {
-       return (1);
-   }
-   else if (y == 1)
-   {
-       return (x);
-   }
+if (y < 0)
+{
+return (-1);
+}
+else if (y == 0)
+{
+return (1);
+}
+else if (y == 1)
+{
+return (x);
+}
 return (x = x * _pow_recursion(x, y - 1));
 }
