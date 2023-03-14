dab0047950e43063e17752e22afbbec008202393
dija <96546980+kadi-ja@users.noreply.github.com>
2023-03-14 15:45:56 +0100

    Create 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*_pow_recursion - a function that returns the factorial of a given number.
+ *@x: data type
+ *@y: data type
+ *Return: factovalue of x power of y 
+ */
+
+int _pow_recursion(int x, int y)
+{
+
+   if (y < 0)
+   {
+       return (-1);
+   }
+  else if (y == 0)
+   {
+       return (1);
+   }
+   else if (y == 1)
+   {
+       return (x);
+   }
+return (x = x * _pow_recursion(x, y - 1));
+}
