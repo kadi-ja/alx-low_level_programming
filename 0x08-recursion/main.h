11f2e5dd598f4d59f53dfcc4ab10c0d0eccc155a
dija <96546980+kadi-ja@users.noreply.github.com>
2023-03-14 16:49:35 +0100

    Update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,6 +7,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
  int helper(int n, int i);
 int is_prime_number(int n);
+int _sqrt(int x, int i);
 int help(int n, int d);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
