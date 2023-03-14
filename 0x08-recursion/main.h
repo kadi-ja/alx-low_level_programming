fafaeb64b8623a24c74634d112a4da455ea1d5e7
dija <96546980+kadi-ja@users.noreply.github.com>
2023-03-14 16:53:37 +0100

    Update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,15 +1,17 @@
 int _putchar(char c);
 void _puts_recursion(char *s);
+void rev_printer(char *s, int n);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
+int counter(int count, char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
- int helper(int n, int i);
+int halp(int c, int i);
+int hai(int n, int d);
 int is_prime_number(int n);
 int _sqrt(int x, int i);
-int help(int n, int d);
+int get_length(char *s);
+int my_pal(char *s, int l);
 int is_palindrome(char *s);
-int my_pal(char *s, int l)
-int get_length(char *s)
 int wildcmp(char *s1, char *s2);
