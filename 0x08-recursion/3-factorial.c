e6800704aed981aa0ebb64c1e2e2bbd95c9194db
dija <96546980+kadi-ja@users.noreply.github.com>
2023-03-14 15:18:36 +0100

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,19 +2,19 @@
 
 /**
  *factorial - a function that returns the factorial of a given number.
- *@s: data type
- Return: factorial 
+ *@n: data type
+ Return: n
  */
 
 int factorial(int n)
 {
 if (n < 0)
 {
-return(-1);
+return (-1);
 }
 if (n == 0)
 {
-return(1);
+return (1);
 }
 n = n * factorial(n - 1);
 return (n);
