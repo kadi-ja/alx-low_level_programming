ae325bc034e1a1af1577b36c212b7bb33019d7ec
dija <96546980+kadi-ja@users.noreply.github.com>
2023-03-17 00:55:18 +0100

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,13 +14,13 @@
 char *create_array(unsigned int size, char c)
 {
 unsigned int i;
- char *a;
- if (size == 0)
-   return (NULL);
- a = malloc(size * sizeof(*a));
- if (a == NULL)
-   return (NULL);
- for (i = 0; i < size; i++)
-   a[i] = c;
- return (a);
+char *a;
+if (size == 0)
+return (NULL);
+a = malloc(size * sizeof(*a));
+if (a == NULL)
+return (NULL);
+for (i = 0; i < size; i++)
+a[i] = c;
+return (a);
 }
