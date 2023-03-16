c6fc0a5d67ad059ff80a7d0091ee86427c6855cd
dija <96546980+kadi-ja@users.noreply.github.com>
2023-03-16 15:43:15 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,6 @@
  *main - a program that prints its name, followed by a new line.
  *@argc: argument count
  *@argv: argument verctor
- *@i: data type
  *Return: always 0
  */
 int main(int argc, char *argv[])
