44436946072d07591d2dad48e7842f90deb22218
dija <96546980+kadi-ja@users.noreply.github.com>
2023-03-20 17:33:18 +0000

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,11 +1,7 @@
 
-#include <stdio.h>
 #ifndef _3_FUNCTION_LIKE_MACRO_H_
 #define _3_FUNCTION_LIKE_MACRO_H_
 
-#define ABS(x) if(x >= 0) \
-return(x); \
-else \
-return(x*(-1));
+#define ABS(x) ((x) < (0) ? -(x) : (x))
 
 #endif
