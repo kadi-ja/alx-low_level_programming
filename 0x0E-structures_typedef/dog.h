ab5d9c5fa95981727ec98b86984b9f6fc9fe5b86
dija <96546980+kadi-ja@users.noreply.github.com>
2023-03-20 20:52:12 +0000

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,16 @@
-#include <stdio.h>
 #ifndef DOG_H
 #define DOG_H
 /**
 * struct dog - a structure of dog's info
-* @name: 1st member is a name 
-* @age: 2nd member is the age 
-* @owner: 3rd member is the owner name 
+* @name: 1st member is a name
+* @age: 2nd member is the age
+* @owner: 3rd member is the owner name
 */
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+char *name;
+float age;
+char *owner;
 };
 
 #endif
