b59d52144a52469ce6908ccf78bc577b238f66d0
kadi-ja <khadija.boustila.96@gmail.com>
2023-03-21 13:27:45 -0700

    4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - a function that creates a new dog.
+ * @name: data type
+ * @age: data type
+ * @owner: data type 
+ * Dog_t: new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+  if (d = malloc(sizeof(struct dog_t)))
+    
+}
