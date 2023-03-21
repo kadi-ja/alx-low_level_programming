5ddddf66918ecf3507ab69cdb90fa012efee700d
dija <96546980+kadi-ja@users.noreply.github.com>
2023-03-21 11:47:52 +0000

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,7 +3,7 @@
 
 
 /**
- *init_dog - a function that initialize a variable of type struct dog
+ * init_dog - a function that initialize a variable of type struct dog
  * @d: pointer to struct dog
  * @name: name
  * @age: age
