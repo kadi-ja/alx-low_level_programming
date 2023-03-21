6615fb0244d8124f5579b212b563fc5857294951
kadi-ja <khadija.boustila.96@gmail.com>
2023-03-21 04:55:50 -0700

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - initializes a variable of type struct dog
+ * @d: pointer to struct dog to initialize
+ * @name: name to initialize
+ * @age: age to initialize
+ * @owner: owner to initialize
+ */
