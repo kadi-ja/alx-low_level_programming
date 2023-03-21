37e122d4d3e51a76e710ce3c90658ed07d0387c8
dija <96546980+kadi-ja@users.noreply.github.com>
2023-03-21 12:41:08 +0000

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,8 +3,8 @@
 #include "dog.h"
 
 /**
- * print_dog - a function that prints a struct dog
- * @d: pointer to struct dog
+ * print_dog - prints a struct dog
+ * @d: struct dog to print
  */
 
 void print_dog(struct dog *d)
@@ -17,5 +17,5 @@ d->name = "(nil)";
 if (d->owner == NULL)
 d->owner = "(nil)";
 
-printf("Name: %S\nAge: %.1f\nOwner: %s\n", d->name, d->age, d->owner);
+printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
