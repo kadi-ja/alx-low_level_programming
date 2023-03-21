057d980ef04e1354c3e9add74796427e317a2e2c
dija <96546980+kadi-ja@users.noreply.github.com>
2023-03-21 12:28:02 +0000

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,9 +12,9 @@ if (d == NULL)
 return;
 
 if  (d->name == NULL)
-d->name = "(nil)"
+d->name = "(nil)";
 if  (d->owner == NULL)
-d->owner = "(nil)"
+d->owner = "(nil)";
 
 printf("Name: %S\nAge: %.1f\nOwner: %s\n", d->name, d->age, d->owner);
 }
