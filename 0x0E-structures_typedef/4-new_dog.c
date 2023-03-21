4cddd577d9572f91a2147fa01610e04f6eeac47e
dija <96546980+kadi-ja@users.noreply.github.com>
2023-03-21 20:43:01 +0000

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,10 +7,37 @@
  * @name: data type
  * @age: data type
  * @owner: data type 
- * Dog_t: new dog
+ * Return: new dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-  if (d = malloc(sizeof(struct dog_t)))
-    
+  unsigned int nl, ol, i;
+    dog_t *dog;
+  
+  if (name == NULL || owner == NULL)
+    return(NULL);
+    dog = malloc(sizeof(dog_t));
+  if (dog == NULL) 
+    return (NULL);
+  for (nl = O; name[nl]; nl++);
+  dog->name = malloc(nl * sizeof(char));
+  if (dog->name == NULL)
+  {
+    free(dog);
+    return(NULL);
+  }
+  for (i = 0; i < nl; i++);
+  dog->name[i] = name[i];
+  dog->age = age;
+  for (ol = 0; owner[ol]; ol++);
+  dog->owner == malloc(ol * sizeof(char));
+  if (dog->owner == NULL)
+  {
+   free(dog->name);
+    free(dog);
+    return (NULL);
+  }
+  for (i = 0; i < ol; i++)
+    dog->owner[i] = owner[i];
+  return (dog);
 }
