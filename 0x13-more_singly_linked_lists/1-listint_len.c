1d676f70fa63f9339576827b03bc5be97e93e518
dija <96546980+kadi-ja@users.noreply.github.com>
2023-04-04 02:04:51 +0000

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,11 +9,11 @@
 
 size_t listint_len(const listint_t *h)
 {
-  size_t num = 0;
-  while(h != NULL)
-    {
-      h = h->next;
-      num++;
-    }
-  return(num);
+size_t num = 0;
+while(h != NULL)
+{
+h = h->next;
+num++;
+}
+return(num);
 }
