d750e028d0e9eb23cbc3e27ae6c3cb88bfa5b506
dija <96546980+kadi-ja@users.noreply.github.com>
2023-04-04 14:59:44 +0000

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,41 +2,42 @@
 #include <stdlib.h>
 
 /**
- * insert_nodeint_at_index - inserting a new node at agiven postion
+ * insert_nodeint_at_index -  a function that inserts a
+ * new node at a given position.
  * @head: double pointer
  * @idx: index of the node
  * @n: new node value
- *
  * Return: the address of new node
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i;
-	listint_t *temp, *new;
+unsigned int i;
+listint_t *end, *beg;
 
-	if (head == NULL)
-		return (NULL);
-	if (idx != 0)
-	{
-		temp = *head;
-		for (i = 0; i < idx - 1 && temp != NULL; i++)
-		{
-			temp = temp->next;
-		}
-		if (temp == NULL)
-			return (NULL);
-	}
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	if (idx == 0)
-	{
-		new->next = *head;
-		*head = new;
-		return (new);
-	}
-	new->next = temp->next;
-	temp->next = new;
-	return (new);
+if (head == NULL)
+return (NULL);
+if (idx != 0)
+{
+end = *head;
+for (i = 0; i < idx - 1 && end != NULL; i++)
+{
+end = end->next;
+}
+if (end == NULL)
+return (NULL);
+}
+beg = malloc(sizeof(listint_t));
+if (beg == NULL)
+return (NULL);
+beg->n = n;
+if (idx == 0)
+{
+beg->next = *head;
+*head = beg;
+return (beg);
+}
+beg->next = end->next;
+end->next = beg;
+return (beg);
 }
