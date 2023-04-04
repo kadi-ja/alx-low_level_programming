ed866a1367a7bc1f0b86f07aab3615eb0fe7b5b3
kadi-ja <Khadija.boustila.96@gmail.com>
2023-04-04 08:09:28 -0700

    fix task

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,12 +11,11 @@ size_t print_listint(const listint_t *h)
 {
 
 size_t num = 0;
-while (h)
-{
-num++;  
-printf("%d\n", h->n);
-h = h->next;
-
-}
-return (num);
+while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
