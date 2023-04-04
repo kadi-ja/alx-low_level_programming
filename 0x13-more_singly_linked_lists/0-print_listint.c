8b779772e2d8829842743128aab9015795d123d5
dija <96546980+kadi-ja@users.noreply.github.com>
2023-04-04 01:48:04 +0000

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,12 +9,12 @@
 
 size_t print_listint(const listint_t *h)
 {
-  size_t num = 0;
-  while(h != NULL)
-    {
-      printf("%d\n", h -> n);
-      h = h -> next;
-      num++;
-         }
-  return(num);
+size_t num = 0;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+num++;
+}
+return (num);
 }
